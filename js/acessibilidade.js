eab0c1ffd074d49fd7046f59c83d61c83a18a1e8
Brunocarpi <165421177+Brunocarpi@users.noreply.github.com>
2024-12-04 01:02:22 -0300

    ajustes (aaaaaaaaaaaaaaaaaaaaaaaaaaa)

--- js/acessibilidade.js
+++ js/acessibilidade.js
@@ -1,13 +1,17 @@
-daltonismoSelect.addEventListener('change', function() {
+document.addEventListener('DOMContentLoaded', function() {
 
-    document.body.classList.remove('protanopia', 'deuteranopia', 'tritanopia');
+    const daltonismoSelect = document.getElementById('daltonismo-select');
     
+    daltonismoSelect.addEventListener('change', function() {
+        document.body.classList.remove('protanopia', 'deuteranopia', 'tritanopia');
+        
+        const selectedValue = daltonismoSelect.value;
+        if (selectedValue) {
+            document.body.classList.add(selectedValue);
+        }
+    });
 
-    const selectedValue = daltonismoSelect.value;
-    if (selectedValue) {
-        document.body.classList.add(selectedValue);
-    }
+ 
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+
 });
-document.body.classList.add(selectedClass);
-
-
